94c3723e9ebceb79857777151a182f825ad7dc35
raphaelboulanger <raphael.boulanger@etu.univ-st-etienne.fr>
2017-05-28 23:02:36 +0200

    Update Section.h

--- Section.h
+++ Section.h
@@ -7,15 +7,17 @@ using namespace std;
 class section
 {
 	int numero_;						// Numero de la section (pour les distinguer et les appeler)
-	int nbrGroupe_;						// Nombre de groupe prÈsent dans cette section
+	int nbrGroupe_;						// Nombre de groupe pr√©sent dans cette section
 	vector<groupe> listeGroupe_;		// vecteur des groupes composant la section
 public:
-	section(int numero);				// Le constructeur ne dÈfinie que numero_ et nbrGroupe_ = 0, la liste des sections sera construite au fur et ‡ mesure
+	section(int numero);				// Le constructeur ne d√©finie que numero_ et nbrGroupe_ = 0, la liste des sections sera construite au fur et √† mesure
 	void append(groupe& unGroupe) { listeGroupe_.push_back(unGroupe); nbrGroupe_++; }
-	void afficherToutLeMonde();			// Permet d'afficher toute la promo (uniquement pour le dÈboggague)
+	void afficherToutLeMonde();			// Permet d'afficher toute la promo (uniquement pour le d√©boggague)
 	groupe getGroupe(int numeroGroupe);
-	int afficherGroupe();				// Affiche le menu de choix de groupe une fois que l'on a selectionnÈ la section, retourne ne numÈro du groupe selectionnÈ
-	// MÈthodes en ligne
+	int afficherGroupe();				// Affiche le menu de choix de groupe une fois que l'on a selectionn√© la section, retourne ne num√©ro du groupe selectionn√©
+	// M√©thodes en ligne
 	int getNbrGroupe() { return nbrGroupe_; }
 	int getNumero() { return numero_; }
 };
+
+// Auteur : Aur√©lien TURPIN
