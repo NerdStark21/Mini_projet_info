a823027413157a60a2889eca605e82fdcea2d9f9
raphaelboulanger <raphael.boulanger@etu.univ-st-etienne.fr>
2017-05-28 23:26:42 +0200

    Update Eleve.h

--- Eleve.h
+++ Eleve.h
@@ -9,13 +9,15 @@ class eleve
 	string nom_;
 	string prenom_;
 	char lettreGroupe_;			// Un seul caractere
-	string photo_;				// L'adresse de la photo assiciÈe
+	string photo_;				// L'adresse de la photo assici√©e
 public:
-	eleve(string nom="", string prenom="", char lettreGroupe='A', string photo=""); // Constructeur basique pour dÈfinir les variables de la classe
-	// MÈthodes en ligne
+	eleve(string nom="", string prenom="", char lettreGroupe='A', string photo=""); // Constructeur basique pour d√©finir les variables de la classe
+	// M√©thodes en ligne
 	string getNom() { return nom_; }
 	string getPrenom() { return prenom_; }
 	char getNumeroGroupe() { return lettreGroupe_; }
 	void getPhoto(string& photo) { photo = photo_; }
 	string getPhoto() { return photo_; }
 };
+
+// Auteur : Aur√©lien TURPIN
